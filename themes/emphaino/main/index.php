4dc57674a74665b14cd4e6500e2e94fe46e066ea
Finger Nailbiter <iam.nailbiter@gmail.com>
2015-11-20 22:01:42 +0700

    Add Download Information

--- themes/emphaino/main/index.php
+++ themes/emphaino/main/index.php
@@ -1,9 +1,9 @@
 <?php if (!defined('FLUX_ROOT')) exit; ?>
-<h2><?php echo htmlspecialchars(Flux::message('MainPageHeading')) ?></h2>
-<p><strong><?php echo htmlspecialchars(Flux::message('MainPageInfo')) ?></strong></p>
-<p><?php echo htmlspecialchars(Flux::message('MainPageInfo2')) ?></p>
+<h2>Download Reckoning RO Client</h2>
+<p><strong>Silahkan pilih salah satu installer yang kami sediakan.</strong></p>
+<p>kami merekomendasikan Full Installer untuk kenyamanan anda bermain.</p>
 <ol>
-	<li><p class="green"><?php echo htmlspecialchars(sprintf(Flux::message('MainPageStep1'), __FILE__)) ?></p></li>
-	<li><p class="green"><?php echo htmlspecialchars(Flux::message('MainPageStep2')) ?></p></li>
+	<li><p class="green"><a href="https://drive.google.com/open?id=0BxSCna1Uel0LOTgxcm5vbEVjaU0"><strong>Lite Installer [64 Mb]</strong></a></p></li>
+	<li><p class="green"><a href="https://drive.google.com/open?id=0BxSCna1Uel0LUVJpOGxLRVBQNzQ"><strong>Full Installer [2.7 Gb]</strong></a></p></li>
 </ol>
-<p style="text-align: right"><strong><em><?php echo htmlspecialchars(Flux::message('MainPageThanks')) ?></em></strong></p>
+<p style="text-align: right"><strong><em>selamat bermain!</em></strong></p>
