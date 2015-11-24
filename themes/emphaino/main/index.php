39d792cdc0dc3d808c496aeb7604f7c5fdb70548
Finger Nailbiter <iam.nailbiter@gmail.com>
2015-11-24 20:48:39 +0700

    Change Flux to SGCP
    Remove Flux & Upload SGCP

--- themes/emphaino/main/index.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('FLUX_ROOT')) exit; ?>
-<h2>Download Reckoning RO Client</h2>
-<p><strong>Silahkan pilih salah satu installer yang kami sediakan.</strong></p>
-<p>kami merekomendasikan Full Installer untuk kenyamanan anda bermain.</p>
-<ol>
-	<li><p class="green"><a href="https://drive.google.com/open?id=0BxSCna1Uel0LOTgxcm5vbEVjaU0"><strong>Lite Installer [64 Mb]</strong></a></p></li>
-	<li><p class="green"><a href="https://drive.google.com/open?id=0BxSCna1Uel0LUVJpOGxLRVBQNzQ"><strong>Full Installer [2.7 Gb]</strong></a></p></li>
-</ol>
-<p style="text-align: right"><strong><em>selamat bermain!</em></strong></p>
